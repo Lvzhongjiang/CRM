e1510602c9e1b35fe408efbbd2d80050e4125422
lvzhongjiang <1561777656@qq.com>
2019-12-05 10:10:56 +0800

    测试

--- src/main/java/com/hy/crm/entity/User.java
+++ src/main/java/com/hy/crm/entity/User.java
@@ -3,8 +3,6 @@ package com.hy.crm.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
 
 /**
  * <p>
