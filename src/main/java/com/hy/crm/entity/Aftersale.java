3e450f9b58feefeb417cf9ad63b360f7677d01c9
renys <2609788249@qq.com>
2019-12-05 09:53:20 +0800

    实体类第二次提交

--- src/main/java/com/hy/crm/entity/Aftersale.java
+++ src/main/java/com/hy/crm/entity/Aftersale.java
@@ -58,5 +58,151 @@ public class Aftersale {
 
     private String attachment;
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public Integer getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(Integer clientid) {
+        this.clientid = clientid;
+    }
+
+    public Integer getContractid() {
+        return contractid;
+    }
+
+    public void setContractid(Integer contractid) {
+        this.contractid = contractid;
+    }
+
+    public String getMaincontent() {
+        return maincontent;
+    }
+
+    public void setMaincontent(String maincontent) {
+        this.maincontent = maincontent;
+    }
+
+    public String getContactperson() {
+        return Contactperson;
+    }
+
+    public void setContactperson(String contactperson) {
+        Contactperson = contactperson;
+    }
+
+    public String getFixedtelephones() {
+        return fixedtelephones;
+    }
+
+    public void setFixedtelephones(String fixedtelephones) {
+        this.fixedtelephones = fixedtelephones;
+    }
+
+    public String getMobiletelephones() {
+        return mobiletelephones;
+    }
+
+    public void setMobiletelephones(String mobiletelephones) {
+        this.mobiletelephones = mobiletelephones;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public Integer getTypeservice() {
+        return typeservice;
+    }
+
+    public void setTypeservice(Integer typeservice) {
+        this.typeservice = typeservice;
+    }
+
+    public Integer getModelservice() {
+        return modelservice;
+    }
+
+    public void setModelservice(Integer modelservice) {
+        this.modelservice = modelservice;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getPersonnel() {
+        return personnel;
+    }
+
+    public void setPersonnel(String personnel) {
+        this.personnel = personnel;
+    }
+
+    public String getServicescore() {
+        return Servicescore;
+    }
+
+    public void setServicescore(String servicescore) {
+        Servicescore = servicescore;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
 }
